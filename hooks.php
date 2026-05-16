b8a0e9f3f6a1e83e8329d65f07f4d70d36583c2b
Kevin Fraser <kevin@ksfraser.com>
2026-05-16 15:31:00 -0600

    Simplify hooks, update config to 2.4.3, move code to common utilities

---
+++ hooks.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * ksf_FA_HRM Module Hooks for FrontAccounting
+ */
+
+define('SS_ksf_FA_HRM', 100 << 8);
+
+class hooks_ksf_FA_HRM extends hooks {
+    var $module_name = 'ksf_FA_HRM';
+
+    function install_extension($check_only=true) {
+        return true;
+    }
+
+    function install_tabs($app) {
+        // Override in modules that add apps
+    }
+
+    function install_options($app) {
+        // Override in modules that add menu items
+    }
+
+    function activate_extension($company, $check_only=true) {
+        $this->ensure_composer_dependencies();
+        return true;
+    }
+
+    function install_access() {
+        $security_sections[SS_ksf_FA_HRM] = _("");
+        $security_areas['SA_ksf_FA_HRMVIEW'] = array(SS_ksf_FA_HRM | 1, _("View "));
+        $security_areas['SA_ksf_FA_HRMMANAGE'] = array(SS_ksf_FA_HRM | 2, _("Manage "));
+        return array($security_areas, $security_sections);
+    }
+    
+    private function ensure_composer_dependencies() {
+        $module_dir = dirname(__FILE__);
+        $autoload_path = $module_dir . '/vendor/autoload.php';
+        
+        if (file_exists($autoload_path)) {
+            return;
+        }
+        
+        $composer_path = $module_dir . '/composer.json';
+        if (!file_exists($composer_path)) {
+            return;
+        }
+        
+        chdir($module_dir);
+        $output = [];
+        $return_code = 0;
+        exec('composer install --no-interaction --prefer-dist 2>&1', $output, $return_code);
+        if ($return_code !== 0) {
+            error_log('KSF Module: composer install failed: ' . implode("\n", $output));
+        }
+    }
+}
