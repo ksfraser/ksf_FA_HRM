5b5537247f885b58d64dd0882f8ff5d4681066a6
Kevin Fraser <kevin@ksfraser.com>
2026-05-17 00:07:45 -0600

    Fix PHP 7.4: remove null coalescing, arrow functions, short arrays in HRM

--- import.php
+++ import.php
@@ -1,10 +1,10 @@
 <?php
 
 $module = 'employee';
-$target_fields = [
+$target_fields = array(
     'emp_no',
     'first_name',
-    'last_name', 
+    'last_name',
     'email',
     'phone',
     'department',
@@ -13,28 +13,28 @@ $target_fields = [
     'joined_date',
     'salary',
     'hourly_rate',
-];
+);
 
-return [
+return array(
     'name' => 'ksf_FA_HRM',
     'title' => 'HRM Import',
     'fields' => $target_fields,
     'processor' => function($row) use ($module) {
         global $db;
         include_once '../../ksf_FA_HRM/includes/db.inc';
-        
-        $emp_no = $row['emp_no'] ?? '';
-        
+
+        $emp_no = isset($row['emp_no']) ? $row['emp_no'] : '';
+
         if (empty($emp_no)) {
             return false;
         }
-        
+
         $check = db_fetch_assoc(db_query(
             "SELECT emp_no FROM " . TB_PREF . "ksf_employee WHERE emp_no = " . db_escape($emp_no)
         ));
-        
+
         if ($check) {
-            $sets = [];
+            $sets = array();
             foreach ($row as $f => $v) {
                 if ($f !== 'emp_no') {
                     $sets[] = "$f = " . db_escape($v);
@@ -43,12 +43,11 @@ return [
             $sql = "UPDATE " . TB_PREF . "ksf_employee SET " . implode(', ', $sets) . " WHERE emp_no = " . db_escape($emp_no);
         } else {
             $cols = implode(', ', array_keys($row));
-            $vals = implode(', ', array_map(fn($v) => db_escape($v), array_values($row)));
+            $vals = implode(', ', array_map(function($v) { return db_escape($v); }, array_values($row)));
             $sql = "INSERT INTO " . TB_PREF . "ksf_employee ($cols) VALUES ($vals)";
         }
-        
+
         db_query($sql);
-        
-        return ['emp_no' => $emp_no, 'action' => $check ? 'updated' : 'created'];
+        return true;
     },
-];
+);
--- install.php
+++ install.php
@@ -2,11 +2,11 @@
 
 $module = 'ksf_FA_HRM';
 
-$access_levels = [
-    ['HRM', 'ksf_hrm', FA_PERMISSION_READ],
-];
+$access_levels = array(
+    array('HRM', 'ksf_hrm', FA_PERMISSION_READ),
+);
 
-extends_hook('install_ksf_fa_hrm', 'Ksf\\FA\\HRM\\Hooks\\InstallHook::install');
-extends_hook('uninstall_ksf_fa_hrm', 'Ksf\\FA\\HRM\\Hooks\\InstallHook::uninstall');
+extends_hook('install_ksf_fa_hrm', 'Ksf\FA\HRM\Hooks\InstallHook::install');
+extends_hook('uninstall_ksf_fa_hrm', 'Ksf\FA\HRM\Hooks\InstallHook::uninstall');
 
-add_extension_options_widget('ksf_FA_HRM', 'Ksf\\FA\\HRM\\Hooks\\InstallHook::install');
+add_extension_options_widget('ksf_FA_HRM', 'Ksf\FA\HRM\Hooks\InstallHook::install');
